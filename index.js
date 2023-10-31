66eddc0c65cf38a8081d74345cd4a6cb87811d6b
Param9478 <singh.parminder9478@gmail.com>
2023-10-31 12:11:20 -0600

    adding frontend to backend

--- index.js
+++ index.js
@@ -1,101 +1,69 @@
+// const http = require("http");
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-
 app.use(express.json());
 
-let phonebook = [
+const cors = require("cors");
+app.use(cors());
+app.use(express.static("build"));
+
+let notes = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    content: "HTML is easy",
+    important: true,
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    content: "Browser can execute only JavaScript",
+    important: false,
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
   },
 ];
 
 app.get("/", (request, response) => {
   response.send(`<h1>Hello World</h1>`);
 });
-app.get(`/info`, (request, response) => {
-  response.send(`Phonebook has info for ${phonebook.length} people <br/>
-  ${new Date()}
-  `);
+
+app.get(`/api/notes`, (request, response) => {
+  response.json(notes);
 });
 
-app.get(`/api/phonebook`, (request, response) => {
-  response.json(phonebook);
-});
-
-app.get(`/api/phonebook/:id`, (request, response) => {
+app.get(`/api/notes/:id`, (request, response) => {
   const id = Number(request.params.id);
-  const person = phonebook.find((p) => p.id === id);
-  person
-    ? response.json(person)
-    : response.status(404).json({
-        error: "No data found",
-      });
+  const note = notes.find((note) => note.id === id);
+  note ? response.json(note) : response.status(404).end();
 });
 
-app.delete(`/api/phonebook/:id`, (request, response) => {
+app.delete(`/api/notes/:id`, (request, response) => {
   const id = Number(request.params.id);
-  phonebook = phonebook.filter((person) => person.id !== id);
+  notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
 });
-
 const generateId = () => {
-  const maxId =
-    phonebook.length > 0 ? Math.max(...phonebook.map((p) => p.id)) : 0;
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
 };
-
-morgan.token("post-data", (req, res) => {
-  return JSON.stringify(req.body);
-});
-
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :post-data"
-  )
-);
-
-app.post(`/api/phonebook`, (request, response) => {
+app.post(`/api/notes`, (request, response) => {
   const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response.status(404).json({
-      error: "Content Missing",
+  if (!body.content) {
+    return response.status(400).json({
+      error: "Content missing",
     });
   }
-  const person = {
+  const note = {
+    content: body.content,
+    important: body.important || false,
     id: generateId(),
-    name: body.name,
-    number: body.number,
   };
-  const isNameExist = phonebook.find((p) => p.name === body.name);
-  console.log(isNameExist);
-  isNameExist
-    ? response.status(404).json({
-        error: `${body.name} already exists`,
-      })
-    : (phonebook = phonebook.concat(person));
-  response.json(phonebook);
+  notes = notes.concat(note);
+  console.log(note);
+  response.json(note);
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
