848d6358b0367da944297cf3a17298f429c71e63
Param9478 <singh.parminder9478@gmail.com>
2023-11-02 12:05:40 -0600

    fixing error in vite-react

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const http = require("http");
 const express = require("express");
 const app = express();
-app.use(express.static("build"));
+app.use(express.static("dist"));
 app.use(express.json());
 
 const cors = require("cors");
